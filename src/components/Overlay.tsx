d11598fd4ad1b5cd84a6028f23dc3ffcc4f454e0
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-07-03 23:28:02 +0530

    Add breaktime via props

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,17 +1,25 @@
-import { PropsWithChildren, ReactNode } from "react";
+import { PropsWithChildren, ReactNode, useContext } from "react";
+import { Context } from "../store/context";
+import useInterval from "../util/useInterval";
+import useTime from "../util/useTime";
 import Backdrop from "./Backdrop";
+import Timer from "./Timer";
 
-const Overlay: React.FC<PropsWithChildren & {breakType:string}> = (props) =>{
+const Overlay: React.FC<PropsWithChildren & {breakType:string,breakTime:number}> = (props) =>{
+
+    const hideNotif = useInterval(props.breakType)
 
 
     return(
        <Backdrop>
          <div className="overlay">
             <h2>Time to {props.breakType}</h2>
-
+            <Timer breakTime={props.breakTime}/>
             <img src={require('../logo.svg').default} height="200" width="200" alt="a11y"/>
 
-            <desc>Lorem ipsum dumb text</desc>
+            <article>Lorem ipsum dumb text</article>
+
+            <button onClick={()=>hideNotif()}>Dismiss</button>
         </div>
        </Backdrop>
     )
