51802130b880ffd2b60c689d6cbcf7e1b170d6a5
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-07-08 22:35:13 +0530

    Add data.ts

---
+++ src/util/data.ts
@@ -0,0 +1,16 @@
+export const data = {
+    "blink": {
+        "title": "Blink",
+        "description": "The rule of 20/20/20 says that for every 20 minutes\
+        spent looking at a screen, a person should look at something 20 feet\
+        away for 20 seconds."
+    },
+    "water":{
+        "title": "drink water",
+        "description": "Consider drinking a glass of water to keep yourself hydrated."
+    },
+    "stretch":{
+        "title":" Stretch",
+        "description":"Sitting is the new smoking. Kindly take a small break to stertch your body and move around."
+    }
+}
