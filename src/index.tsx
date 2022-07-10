3cc21023b8a1052fe6f1e20df679aa1f04be0af3
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-07-10 16:19:43 +0530

    Add replaceCSS: false props to wrapper component

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ContextWrapper from './store/ContextWrapper';
+import { ChakraProvider } from '@chakra-ui/react'
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ContextWrapper>
-      <App/>
-    </ContextWrapper>
+    <ChakraProvider resetCSS={false}>
+      <ContextWrapper>
+        <App />
+      </ContextWrapper>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
