ef4d6d9267677763b2b678d9f67bb50a6f6fe775
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-07-14 22:01:30 +0530

    Update config changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require('webpack');
 
 module.exports = {
 
@@ -11,29 +12,65 @@ module.exports = {
     mode: 'production',
     output:{
         path: path.join(__dirname, 'dist'),
-        filename: '[name].js'
+        filename: '[name].js',
+        publicPath: '/static/',
     },
     module: {
         rules: [
             {
                 test: /\.(ts|tsx)$/,
                 exclude: /node_modules/,
-                use: 'babel-loader'
+                use: ['babel-loader']
                 
             },
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html',
-            filename: 'index.html'
+            filename: 'index.html',
+        }),
+        new CopyPlugin({
+            patterns: [ { from: './public/manifest.json', to: 'manifest.json' },]
+        }),
+        new CopyPlugin({
+            patterns: [ { from: './public/logo192.png', to: path.join(__dirname,'dist') },]
+        }),
+        new webpack.ProvidePlugin({
+            'React':'react',
         })
+     
     ],
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json']
+   
+    resolve:{
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+       
+    },
+
+    externals:{
+        react: {
+            commonjs: "react",
+            commonjs2: "react",
+            amd: "React",
+            root: "React"
+        },
+        "react-dom":{
+            commonjs:"react-dom",
+            commonjs2:"react-dom",
+            amd:"ReactDOM",
+            root:"ReactDOM"
+        }
     }
 }
